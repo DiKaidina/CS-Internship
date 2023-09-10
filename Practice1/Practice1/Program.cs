a38f193a40a476072b09dc606c7c25141882fc58
Diana Kaidina <missdina1605@mail.ru>
2023-09-10 13:08:28 +0600

    Week1 Practice1

---
+++ Practice1/Practice1/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите ваше ФИО");
+        string username = Console.ReadLine();
+
+        Console.WriteLine("Приветству вас," + username + "!");
+
+        //Ввод нескольких чисел и их сумма
+
+        Console.WriteLine("Введите числа через пробел:");
+        string input_nums = Console.ReadLine();
+
+        double sum = CalcSum(input_nums);
+
+        Console.WriteLine($"Сумма чисел: {sum}");
+
+
+    }
+    static double CalcSum(string input)
+    {
+        double sum = 0;
+        string[] numbers = input.Split(' ');
+
+        foreach (string num in numbers)
+        {
+            //TryParse по умолчанию не генерирует исключения
+            if (double.TryParse(num, out double number))
+            {
+                sum += number;
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка: '{num}' не число!");
+            }
+        }
+
+        return sum;
+    }
+ }
