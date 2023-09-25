5ddd5e3888cece1329e916a92f52c3be96268f1e
Diana Kaidina <missdina1605@mail.ru>
2023-09-25 23:08:30 +0600

    Замена Практики 4

---
+++ Practice_4/Practice_4/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_4
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+
+        public string ToString()
+        {
+            return $"Название: {Title}, Автор: {Author}, Год издания: {Year}";
+        }
+    }
+}
---
+++ Practice_4/Practice_4/HomeLibrary.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_4
+{
+    internal class HomeLibrary
+    {
+        private List<Book> books = new List<Book>();
+
+        // добавить книгу 
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        //удалить книгу
+        public void RemoveBook(Book book)
+        {
+            books.Remove(book);
+        }
+
+        // Поиск по автору
+        public List<Book> FindByAuthor(string author)
+        {
+            //сравнение без учета регистра
+            return books.Where(book => book.Author.Equals(author, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        //по году издания
+        public List<Book> FindBooksByYear(int year)
+        {
+            return books.Where(book => book.Year == year).ToList();
+        }
+
+        // сортировка по названию
+        public void SortByTitle()
+        {
+            books = books.OrderBy(book => book.Title).ToList();
+        }
+
+        
+        public void SortByAuthor()
+        {
+            books = books.OrderBy(book => book.Author).ToList();
+        }
+
+        
+        public void SortByYear()
+        {
+            books = books.OrderBy(book => book.Year).ToList();
+        }
+
+        // Метод для вывода всех книг в библиотеке
+        public void DisplayAllBooks()
+        {
+            Console.WriteLine("Список книг в библиотеке:");
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
